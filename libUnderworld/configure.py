b2aa84d4975c26d8d7d81bc105bf76cc9bef335d
jmansour <mansourjohn@gmail.com>
2015-11-06 14:42:01 +1100

    It seems some older versions of git require that 'git submodule update --init' (used to grab lavavu) is run from the git repo top level directory.

--- libUnderworld/configure.py
+++ libUnderworld/configure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import sys, subprocess, shutil, os
-subp = subprocess.Popen('git submodule update --init', shell=True)  #Checkout submodules (LavaVu)
+subp = subprocess.Popen('git submodule update --init', shell=True, cwd=os.path.join(os.path.dirname(__file__), '../'))  #Checkout submodules (LavaVu)
 subp.wait()
 subp = subprocess.Popen(
     'config/scons/scons.py --config=force -f SConfigure ' + ' '.join(sys.argv[1:]), shell=True
