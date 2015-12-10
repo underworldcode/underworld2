ffabc31237460272f0b6325ee2161331d13a7700
jmansour <mansourjohn@gmail.com>
2015-12-10 21:03:26 +1100

    * add docstring * put in try block so that it concludes quietly on any issues.

--- underworld/_net/__init__.py
+++ underworld/_net/__init__.py
@@ -22,20 +22,43 @@ GA_CLIENT_ID = str(uuid.uuid4())
 
 
 def PostGAEvent( category, action, label=None, value=None ):
-    connection = httplib.HTTPSConnection('www.google-analytics.com')
-    """ Posts an Event Tracking message to Google Analytics. """
-    form_fields = {
-    "v"  : "1",             # Version.
-    "aip": "1",             # Enable IP anonymizing
-    "tid": GA_TRACKING_ID,  # Tracking ID / Web property / Property ID.
-    "cid": GA_CLIENT_ID,    # Anonymous Client ID.
-    "t"  : "event",         # Event hit type.
-    "ec" : category,        # Event Category. Required.
-    "ea" : action,          # Event Action. Required.
-    "el" : label,           # Event label.
-    "ev" : value,           # Event value.
-    }
-    params = urllib.urlencode(form_fields)
-    connection.connect()
-    connection.request('POST', '/collect?%s' % params, '', { "Content-Type": "application/x-www-form-urlencoded" })
+    """ 
+    Posts an Event Tracking message to Google Analytics.
+    
+    Full parameter reference may be found here:
+    https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#ev
+    
+    Note, this function will return quietly on any errors. 
+    
+    Parameters
+    ----------
+    category: str
+        Textual name for event category
+    action: str
+        Textual name for event action
+    label: str (optional)
+        Optional label for event
+    value: non-negative integer (optional)
+        Optional value for event
+        
+
+    """
+    try:
+        connection = httplib.HTTPSConnection('www.google-analytics.com')
+        form_fields = {
+        "v"  : "1",             # Version.
+        "aip": "1",             # Enable IP anonymizing
+        "tid": GA_TRACKING_ID,  # Tracking ID / Web property / Property ID.
+        "cid": GA_CLIENT_ID,    # Anonymous Client ID.
+        "t"  : "event",         # Event hit type.
+        "ec" : category,        # Event Category. Required.
+        "ea" : action,          # Event Action. Required.
+        "el" : label,           # Event label.
+        "ev" : value,           # Event value.
+        }
+        params = urllib.urlencode(form_fields)
+        connection.connect()
+        connection.request('POST', '/collect?%s' % params, '', { "Content-Type": "application/x-www-form-urlencoded" })
+    except:
+        pass
 
