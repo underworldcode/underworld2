ec9e6cbecf5018455482aa30f96a67fe24617e32
Owen Kaluza <owen.kaluza@monash.edu>
2016-04-12 15:22:09 +1000

    Update vis configuration, prioritise cgl above glut
    Also added Cocoa/Quartz frameworks for future use

--- libUnderworld/config/packages/CGL.py
+++ libUnderworld/config/packages/CGL.py
@@ -5,6 +5,6 @@ class CGL(Package):
     def gen_envs(self, loc):
         env = self.env.Clone()
         self.headers = ['OpenGL/OpenGL.h', 'OpenGL/gl.h']
-        env.AppendUnique(FRAMEWORKS=['OpenGL'])
+        env.AppendUnique(FRAMEWORKS=['OpenGL', 'Cocoa', 'Quartz'])
         env.AppendUnique(CPPPATH=['/System/Library/Frameworks/OpenGL.framework/Headers'])
         yield env
