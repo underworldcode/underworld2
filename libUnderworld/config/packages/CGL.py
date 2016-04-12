1d9e1fb2c5fb0485974e08cf9ff3c5e914b1bde8
Owen Kaluza <owen.kaluza@monash.edu>
2016-04-12 10:02:10 +1000

    Updated offscreen rendering context on Mac OS X

--- libUnderworld/config/packages/CGL.py
+++ libUnderworld/config/packages/CGL.py
@@ -1,11 +1,10 @@
 from config import Package
 
-class AGL(Package):
+class CGL(Package):
 
     def gen_envs(self, loc):
         env = self.env.Clone()
-        self.headers = ['AGL/agl.h', 'gl.h']
-        env.AppendUnique(FRAMEWORKS=['AGL'])
+        self.headers = ['OpenGL/OpenGL.h', 'OpenGL/gl.h']
         env.AppendUnique(FRAMEWORKS=['OpenGL'])
         env.AppendUnique(CPPPATH=['/System/Library/Frameworks/OpenGL.framework/Headers'])
         yield env
