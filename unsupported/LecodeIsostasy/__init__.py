91bd61e03ca7bfce52f620886a0754a601cb633d
Romain Beucher <rbeucher@unimelb.edu.au>
2017-04-24 16:46:52 +1000

    Add LecodeIsostasy unsupported

---
+++ unsupported/LecodeIsostasy/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1"
+
+import warnings
+
+warnings.warn(
+"""\n
+The LecodeIsostasy module is not supported.\n
+Questions should be addressed to romain.beucher@unimelb.edu.au \n """
+)
+
+from LecodeIsostasy import lecode_tools_isostasy
+
