298a3323725440dbc3761268df44d02a99fee106
Romain Beucher <rbeucher@unimelb.edu.au>
2017-05-02 16:03:44 +1000

    Add Huismans Beaumont 2007 plasticity

--- unsupported/rheology/yielding_criteria.py
+++ unsupported/rheology/yielding_criteria.py
@@ -17,3 +17,15 @@ Yield_mantle_Rey2010 = {"Cohesion": 10. * u.megapascal,
                         "FrictionCoef": 0.577,
                         "FrictionCoefSw": 0.1154,
                         "MinimumViscosity": 1e19 * u.pascal * u.second}
+
+Yield_WeakCrust_HuismansBeaumont2007 = {"Cohesion": 20. * u.megapascal,
+                        "CohesionSw": 20. * u.megapascal,
+                        "FrictionCoef": 0.123,
+                        "FrictionCoefSw": 0.017,
+                        "MinimumViscosity": 1e19 * u.pascal * u.second}
+
+Yield_WeakCrust_HuismansBeaumont2007 = {"Cohesion": 20. * u.megapascal,
+                        "CohesionSw": 20. * u.megapascal,
+                        "FrictionCoef": 0.123,
+                        "FrictionCoefSw": 0.017,
+                        "MinimumViscosity": 1e19 * u.pascal * u.second}
