c25e5fba7094d79522e92760730211514b2a84f1
Dan Sandiford <sonderfjord@gmail.com>
2017-05-07 23:47:52 +1000

    nearest neighbour / IDW interpolation using scipy kdTree

---
+++ unsupported/utilities/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.1"
+
+
+import warnings
+
+warnings.warn(
+"""\n
+The alchemy module is not supported.
+Questions should be addressed to sandd@student.unimelb.edu.au \n """
+)
+
+
+from interpolation import *
