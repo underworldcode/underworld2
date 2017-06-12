fc4964a67c0a096350b1c7085fe10ae829eb78c8
Romain Beucher <rbeucher@unimelb.edu.au>
2017-06-13 05:44:47 +1000

    Output warning message only once when running in parallel

--- unsupported/LecodeIsostasy/__init__.py
+++ unsupported/LecodeIsostasy/__init__.py
@@ -1,12 +1,14 @@
 __version__ = "0.1"
 
+from underworld import rank
 import warnings
 
-warnings.warn(
-"""\n
-The LecodeIsostasy module is not supported.\n
-Questions should be addressed to romain.beucher@unimelb.edu.au \n """
-)
+if rank() == 0:
+    warnings.warn(
+    """\n
+    The LecodeIsostasy module is not supported.\n
+    Questions should be addressed to romain.beucher@unimelb.edu.au \n """
+    )
 
 from LecodeIsostasy import lecode_tools_isostasy
 
--- unsupported/scaling/__init__.py
+++ unsupported/scaling/__init__.py
@@ -1,14 +1,16 @@
 __version__ = "0.1"
 
+from underworld import rank
 import warnings
 
-warnings.warn(
-"""\n
-The scaling module is not supported.\n
-It requires 'pint' as a dependency.\n
-You can install pint by running:\n
-'pip install pint' in a terminal\n
-Questions should be addressed to romain.beucher@unimelb.edu.au \n """
-)
+if rank() == 0: 
+    warnings.warn(
+    """\n
+    The scaling module is not supported.\n
+    It requires 'pint' as a dependency.\n
+    You can install pint by running:\n
+    'pip install pint' in a terminal\n
+    Questions should be addressed to romain.beucher@unimelb.edu.au \n """
+    )
 
 from scaling import *
