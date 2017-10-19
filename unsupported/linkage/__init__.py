86ef0338d18544b22687fa4642789f699c6ea779
Romain Beucher <rbeucher@unimelb.edu.au>
2017-10-19 13:33:39 +1100

    Add 'linkage' module to unsupported

---
+++ unsupported/linkage/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.1"
+
+from underworld import rank
+import warnings
+
+if rank() == 0: 
+    warnings.warn(
+    """\n
+    The 'linkage' module is not supported.\n
+    It requires 'badlands' as a dependency.\n
+    Questions should be addressed to romain.beucher@unimelb.edu.au \n """
+    )
+
+from linkage import *
