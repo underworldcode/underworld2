87bd52e479cfdceec1eb30c0cfd5e2ce732cb0e4
julian <house.of.jules@gmail.com>
2018-01-30 15:55:53 +1100

    Updating the nci_raijin.sh recipe & updatePyPath.sh

--- updatePyPath.sh
+++ updatePyPath.sh
@@ -1,3 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 # source this file to update your pythonpaths
-export PYTHONPATH=$PWD:$PWD/glucifer:$PYTHONPATH
+#
+#  Usage:
+#    source ./updatePyPath.sh or . ./updatePyPath.sh
+
+export PYTHONPATH=$0:$0/glucifer:$PYTHONPATH
