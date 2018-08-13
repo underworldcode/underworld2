1e510ff718e3746e84c151e990a519bb722e8a07
julian <house.of.jules@gmail.com>
2018-08-13 14:29:56 +1000

    Upgrading the example
    Adding a conditionally non linear conductivity.

--- docs/development/models_inprogress/uw3/poisson3D.py
+++ docs/development/models_inprogress/uw3/poisson3D.py
@@ -16,6 +16,11 @@ my_k = fn.math.sin( PI * ( fn_r / 5. - 1. ) )
 
 someFn = fn.misc.constant(-1)
 fn_r = fn.math.sqrt( fn.math.dot( fn.coord(), fn.coord() ) )
-fn_r2 = fn.math.dot( fn.coord(), fn.coord() )
+fn_r2 = (10.*temperature+1.)*fn_r
 
-model.Solve(fn_k=fn_r, fn_f=1., fn_dBC_inner=2., fn_dBC_outer=0.)
+benFn = fn.branching.conditional( [ (fn.coord()[2] > 0.0, fn_r),
+                                    (True               , fn_r2) ] )
+
+#model.Solve(fn_k=fn_r, fn_f=1., fn_dBC_inner=2., fn_dBC_outer=0.)
+
+model.Solve(fn_k=benFn, fn_f=1., fn_dBC_inner=2., fn_dBC_outer=0.)
