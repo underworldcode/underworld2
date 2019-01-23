a22c4205db248c60f07ba90e965b54eb3bfc39e5
jmansour <mansourjohn@gmail.com>
2019-01-24 08:52:28 +1100

    * 1_04 blankenbach moved to models library * simple quantitative test for slab subduction * tweaks for xdmf section of utilies * set new version id

--- underworld/__init__.py
+++ underworld/__init__.py
@@ -30,7 +30,7 @@ is very well suited to complex fluids which is how the solid Earth behaves
 on a geological timescale.
 """
 
-__version__ = "2.7.0-dev"
+__version__ = "2.7.0b"
 
 # lets  set sys.path such that the project parent directory takes
 # precedence
