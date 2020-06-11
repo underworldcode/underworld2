093c188fbec41478e9c0dcc5e090904a460eacf4
Romain Beucher <rbeucher@thermo-geology.fr>
2020-06-11 15:03:26 +1000

    test

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd underworld/libUnderworld
-./configure.py --prefix=$PREFIX || (cat config.log && exit 1)
+./configure.py --prefix=$PREFIX || (cat config.log && ls /lib exit 1)
 ./compile.py
 ./scons.py install 
