ae190e6b63cfe974bb465cba366c3567f87fae37
Romain Beucher <romain.beucher@anu.edu.au>
2020-06-25 14:07:40 +1000

    Try CONDA_PREFIX env var

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
+export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
 
 cd underworld/libUnderworld
-./configure.py --prefix=$PREFIX || (cat config.log && ls /lib exit 1)
+./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
 ./compile.py
 ./scons.py install 
