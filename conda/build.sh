2d562f0fcf7e076d5721379e9c5fdbbe08259536
Romain Beucher <romain.beucher@anu.edu.au>
2020-06-25 15:39:09 +1000

    Try

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
+#export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
 
 cd underworld/libUnderworld
-./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
+#./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
+./configure.py || (cat config.log && ls /lib exit 1)
 ./compile.py
 ./scons.py install 
