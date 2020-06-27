ff9d659495e586fdc5bc2732f8be482c729e41e9
jmansour <mansourjohn@gmail.com>
2020-06-27 12:14:23 +1000

    take 8

--- conda/build.sh
+++ conda/build.sh
@@ -2,6 +2,7 @@
 #export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
 
 find /usr/local -name Python.h
+python -c "from distutils.sysconfig import get_python_inc, get_python_lib; print(get_python_inc()); print(get_python_lib())"
 cd underworld/libUnderworld
 #./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
 ./configure.py || (cat config.log && ls /lib exit 1)
