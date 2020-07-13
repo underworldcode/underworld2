bbe3be562489c34bd556c3b97f78db49e130ec4b
Romain Beucher <romain.beucher@anu.edu.au>
2020-07-13 11:48:55 +1000

    take 11

--- conda/build.sh
+++ conda/build.sh
@@ -2,6 +2,7 @@
 #export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
 
 cd underworld/libUnderworld
+export PETSC_DIR=$PREFIX
 #./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
 ./configure.py || (cat config.log && ls /lib exit 1)
 ./compile.py
