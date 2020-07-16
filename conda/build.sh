45634f71fb242f3f537b49f40fff28eda9505714
Romain Beucher <romain.beucher@anu.edu.au>
2020-07-16 14:00:16 +1000

    try with pip

--- conda/build.sh
+++ conda/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
 
-cd underworld/libUnderworld
 export PETSC_DIR=$PREFIX
-#./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
-./configure.py || (cat config.log && ls /lib exit 1)
-./compile.py
-./scons.py install 
+pip install .
