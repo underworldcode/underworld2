7d3adbd01446a525ab1e5537cf4a70b2a66392d9
jmansour <mansourjohn@gmail.com>
2020-07-16 02:32:09 +0000

    Take 26.
    Adding paths for libxml2 in conda.

--- underworld/libUnderworld/config/packages/libXML2.py
+++ underworld/libUnderworld/config/packages/libXML2.py
@@ -3,6 +3,14 @@ from config import Package
 
 class libXML2(Package):
 
+    def gen_locations(self):
+        # add for conda
+        if 'CONDA_DEFAULT_ENV' in os.environ:
+            conda_env = os.environ['CONDA_DEFAULT_ENV']
+            incpath1 = os.path.join(conda_env,'include','libxml2')
+            incpath2 = os.path.join(conda_env,'include')
+            libpath = os.path.join(conda_env,'lib')
+            yield (conda_env, [incpath1,incpath2], [libpath])
     def gen_base_extensions(self):
         for e in Package.gen_base_extensions(self):
             yield e
@@ -10,8 +18,7 @@ class libXML2(Package):
 
     def gen_envs(self, loc):
         for env in Package.gen_envs(self, loc):
-            # the following is required to force libxml2 to not use the cpp-api
-            env.AppendUnique(CPPDEFINES=["U_SHOW_CPLUSPLUS_API=0",])
+            env.MergeFlags('-DU_SHOW_CPLUSPLUS_API=0')
             self.headers = [os.path.join('libxml', 'parser.h')]
             if self.find_libraries(loc[2], 'xml2'):
                 env.PrependUnique(LIBS=['xml2'])
