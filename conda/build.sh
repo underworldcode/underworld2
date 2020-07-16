dfcf88cca723c8231ebd76004200745de2fede00
Romain Beucher <romain.beucher@anu.edu.au>
2020-07-16 14:05:53 +1000

    Revert "try with pip"
    This reverts commit 45634f71fb242f3f537b49f40fff28eda9505714.

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$CONDA_PREFIX/lib"
 
+cd underworld/libUnderworld
 export PETSC_DIR=$PREFIX
-pip install .
+#./configure.py --prefix=$CONDA_PREFIX || (cat config.log && ls /lib exit 1)
+./configure.py || (cat config.log && ls /lib exit 1)
+./compile.py
+./scons.py install 
