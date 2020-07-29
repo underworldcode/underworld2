3371bd12811f663e6478e11268679f38ca372993
jmansour <mansourjohn@gmail.com>
2020-07-30 07:12:45 +1000

    * Minor tweak for ReadTheDocs pages.

--- underworld/swarm/_swarmvariable.py
+++ underworld/swarm/_swarmvariable.py
@@ -21,9 +21,8 @@ import os
 import weakref
 
 class SwarmVariable(_stgermain.StgClass, function.Function):
-
     """
-    The SwarmVariable class allows users to add data to swarm particles.  The data
+    The SwarmVariable class allows users to add data to swarm particles. The data
 	can be of type "char", "short", "int", "long, "float" or "double".
 
     Note that the swarm allocates one block of contiguous memory for all the particles.
