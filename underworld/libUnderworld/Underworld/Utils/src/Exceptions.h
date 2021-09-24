3f873c9227048b936c82465160f093bce7740c62
Artshap <49969695+Artshap@users.noreply.github.com>
2021-09-24 17:02:26 +1000

    removed extra files

--- underworld/libUnderworld/Underworld/Utils/src/Exceptions.h
+++
@@ -1,7 +0,0 @@
-#ifndef __Underworld_Utils_Exceptions_h__
-#define __Underworld_Utils_Exceptions_h__
-
-int Underworld_fetestexcept();
-int Underworld_feclearexcept();
-
-#endif
--- underworld/libUnderworld/fast-scons.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./scons.py --max-drift=1 --implicit-deps-unchanged $*
