eb6c5866148cb5564c24e925c63e896af9234cdd
Romain Beucher <romain.beucher@anu.edu.au>
2021-12-06 14:14:01 +1100

    Cleaning

--- updatePyPath.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# source this file to update your pythonpaths
-#
-#  Usage:
-#    source ./updatePyPath.sh or . ./updatePyPath.sh
-
-export PYTHONPATH=$PWD:$PWD/underworld/libUnderworld/build/lib:$PYTHONPATH
