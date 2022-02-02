4114b60e2d8d1f56eac1602b16e36670a2582cc2
julian <house.of.jules@gmail.com>
2022-02-02 15:27:37 +1100

    Last bit for release 2.12.1
    - fixing pypi manifest for CMake.
    - updating numpy dependecy for pip, numpy>=1.22.1.
    - incrementing minor release number.

--- setup.py
+++ setup.py
@@ -264,7 +264,7 @@ class BuildExtension(build_ext):
 metadata = {
     'provides' : ['underworld'],
     'zip_safe' : False,
-    'install_requires' : ['numpy','mpi4py>=1.2.2', 'h5py', 'pint']
+    'install_requires' : ['numpy>=1.22.1','mpi4py>=1.2.2', 'h5py', 'pint']
 }
 classifiers = """
 Development Status :: 5 - Production/Stable
