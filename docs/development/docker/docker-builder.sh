9fe7fa314fe2d8b2bff2e386855d9226636abd8b
julian <house.of.jules@gmail.com>
2022-11-18 16:39:09 +1100

    Touch Lavavu

--- docs/development/docker/docker-builder.sh
+++ docs/development/docker/docker-builder.sh
@@ -14,12 +14,12 @@ echo "************************************************************\n"
 docker build . -f ./docs/development/docker/mpi/Dockerfile.openmpi -t julesg/openmpi:4.1.4
 docker tag julesg/openmpi:4.1.4 julesg/openmpi:4.1.4-$ARCH
 
-docker build . -f ./docs/development/docker/petsc/Dockerfile -t julesg/petsc:3.18.0
-docker tag julesg/petsc:3.18.0 julesg/petsc:3.18.0-$ARCH
+docker build . -f ./docs/development/docker/petsc/Dockerfile -t julesg/petsc:3.18.1
+docker tag julesg/petsc:3.18.1 julesg/petsc:3.18.1-$ARCH
 
 docker build . -f ./docs/development/docker/lavavu/Dockerfile -t julesg/lavavu
 docker tag julesg/lavavu julesg/lavavu-$ARCH
 
-docker build . -f ./docs/development/docker/underworld2/Dockerfile -t julesg/underworld2:2.13-$ARCH
-docker tag . julesg/underworld2:2.14.0 julesg/underworld2:2.14.0-$ARCH
+# docker build . -f ./docs/development/docker/underworld2/Dockerfile -t julesg/underworld2:2.13-$ARCH
+# docker tag . julesg/underworld2:2.14.0 julesg/underworld2:2.14.0-$ARCH
 
