5f0b695ba0244d8e15585000a6fc37273a9353d1
julian <house.of.jules@gmail.com>
2022-11-23 18:01:25 +1100

    Update to underworldcode docker hub

--- docs/development/docker/docker-builder.sh
+++ docs/development/docker/docker-builder.sh
@@ -11,10 +11,10 @@ ARCH=$(uname -m)
 echo "Will build docker image locally for architecture type: $ARCH"
 echo "************************************************************\n"
 
-docker build . --pull -f ./docs/development/docker/mpi/Dockerfile.openmpi -t julesg/openmpi:4.1.4-$ARCH
+docker build . --pull -f ./docs/development/docker/mpi/Dockerfile.openmpi -t underworldcode/openmpi:4.1.4-$ARCH
 
-docker build . --pull -f ./docs/development/docker/petsc/Dockerfile -t julesg/petsc:3.18.1-$ARCH
+docker build . --pull -f ./docs/development/docker/petsc/Dockerfile -t underworldcode/petsc:3.18.1-$ARCH
 
-docker build . --pull -f ./docs/development/docker/lavavu/Dockerfile -t julesg/lavavu:$ARCH
+docker build . --pull -f ./docs/development/docker/lavavu/Dockerfile -t underworldcode/lavavu:$ARCH
 
-docker build .  -f ./docs/development/docker/underworld2/Dockerfile -t julesg/underworld2:2.14.0b-$ARCH
+docker build .  -f ./docs/development/docker/underworld2/Dockerfile -t underworldcode/underworld2:2.14.0b-$ARCH
