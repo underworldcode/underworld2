6f1f0ca356494642ccb36df614b78d5f7a65b07d
julian <house.of.jules@gmail.com>
2022-12-02 14:47:49 +1100

    Smarts for docker-builder.sh

--- docs/development/docker/docker-builder.sh
+++ docs/development/docker/docker-builder.sh
@@ -5,6 +5,8 @@ set -e
 
 # Example Usage:
 #   - Run from underworld2 repository head
+#   - mpi and lavavu dockers are automatically generated via github actions
+#   - petsc and underworld2 must be created by runn the following script.
 
 
 ARCH=$(uname -m)
@@ -16,11 +18,25 @@ echo "************************************************************\n"
 #docker build . --pull -f ./docs/development/docker/lavavu/Dockerfile -t underworldcode/lavavu:$ARCH
 
 docker build . --pull \
-  -f ./docs/velopment/docker/petsc/Dockerfile \
-  --build-arg MPI_IMAGE "underworldcode/openmpi:4.1.4" \
+  -f ./docs/development/docker/petsc/Dockerfile \
+  --build-arg MPI_IMAGE="underworldcode/openmpi:4.1.4" \
   -t underworldcode/petsc:3.18.1-$ARCH
 
-docker build . --pull \ 
-  --build-arg PETSC_IMAGE "underworldcode/petsc:3.18.1" \
+# don't use pull here as we want the petsc image above
+docker build . \
+  --build-arg PETSC_IMAGE="underworldcode/petsc:3.18.1-$ARCH" \
   -f ./docs/development/docker/underworld2/Dockerfile \
   -t underworldcode/underworld2:2.14.0b-$ARCH
+
+
+docker push underworldcode/petsc:3.18.1-$ARCH 
+docker push underworldcode/underworld2:2.14.0b-$ARCH
+
+#### if updates for both arm64 and x86_64 build manifest, ie
+# docker manifest create underworldcode/petsc:3.18.1 \
+#  -a underworldcode/petsc:3.18.1-x86_64 \
+#  -a underworldcode/petsc:3.18.1-arm64
+#
+# docker manifest push underworldcode/petsc:3.18.1
+#
+# in future this should be automated
