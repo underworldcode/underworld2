d9106ca77b40a6d77833646536881f27f88a6dd7
julian <house.of.jules@gmail.com>
2023-03-01 16:01:26 +1100

    Fixing some test.
    nbmark and matplotlib are needed for the tests.

--- docs/pytests/test.py
+++ docs/pytests/test.py
@@ -3,22 +3,23 @@ import pytest
 import glob
 import ntpath
 import sys
-import underworld as uw
 from inspect import getsourcefile
+
 
 wdir = ntpath.dirname(getsourcefile(lambda:0))+"/../test/"
 
+# get python scripts to test
 pyscripts = [pytest.param(path, id=ntpath.basename(path)) for path in sorted(glob.glob(wdir+"/*.py"))]
 
 @pytest.mark.parametrize('pyscript', pyscripts)
 def test_python_execution(pyscript):
-    cp = subprocess.run([sys.executable, pyscript])
+    cp = subprocess.run([sys.executable, pyscript], check=True)
     assert cp.returncode == 0
 
+# get ipynb scripts to test
 ipynbscripts = [pytest.param(path, id=ntpath.basename(path)) for path in sorted(glob.glob(wdir+"/*.ipynb"))]
-
 
 @pytest.mark.parametrize('ipynbscript', ipynbscripts)
 def test_ipynb_execution(ipynbscript):
-    cp = subprocess.run(["pytest", "--nbmake", ipynbscript])
+    cp = subprocess.run(["pytest", "--nbmake", ipynbscript], check=True)
     assert cp.returncode == 0
