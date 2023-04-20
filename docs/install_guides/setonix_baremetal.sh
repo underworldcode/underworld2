75bdcaaccd95cb5cb97f5f82f1f56390dddb895f
julian <house.of.jules@gmail.com>
2023-04-20 17:22:45 +1000

    updating the setonix jobs runners

--- docs/install_guides/setonix_baremetal.sh
+++ docs/install_guides/setonix_baremetal.sh
@@ -1,19 +1,30 @@
 #!/bin/bash -l
+ 
+## User required input
+#SBATCH --account=pawsey0407
+#SBATCH --job-name=bobthejob
+#SBATCH --ntasks=3
+#SBATCH --time=00:20:00
 
-# The following assumes read access to 
-#   /software/projects/pawsey0407/setonix/
+## Setup job conditions and run environment
+#SBATCH --ntasks-per-node=64  # found this is needed ~Apr2023
+#SBATCH --cpus-per-task=1     # OMP_NUM_THREADS equivalent
 
-module load python/3.9.15 py-mpi4py/3.1.2-py3.9.15 py-numpy/1.20.3 py-h5py/3.4.0 py-cython/0.29.24
+# Note we avoid any inadvertent OpenMP threading by setting
+export OMP_NUM_THREADS=1
 
+# load system packages: py39, mpi, hdf5 
+module load python/3.9.15 py-mpi4py/3.1.2-py3.9.15 py-numpy/1.20.3 py-h5py/3.4.0 py-cython/0.29.24 cmake/3.21.4
+
+# add custom virtual environment and underworld
 export OPT_DIR=/software/projects/pawsey0407/setonix/
+export PYTHONPATH=$OPT_DIR/py39/lib/python3.9/site-packages/:$PYTHONPATH
+export PYTHONPATH=$OPT_DIR/underworld/2.14.2/lib/python3.9/site-packages:$PYTHONPATH
 
-## for modifying the venv
-# source $OPT_DIR/py39/bin/activate
+# load custom petsc
+export PETSC_DIR=$OPT_DIR/petsc-3.19.0
+export PYTHONPATH=$PETSC_DIR/lib:$PYTHONPATH
 
-## For development only
-# export PETSC_DIR=$OPT_DIR/petsc-3.18.1
-# export PYTHONPATH=$PETSC_DIR/:$PYTHONPATH
-
-# for using the venv
-export PYTHONPATH=$OPT_DIR/py39/lib/python3.9/site-packages:$PYTHONPATH
-export PYTHONPATH=$OPT_DIR/underworld/2.14.2/lib/python3.9/site-packages/:$PYTHONPATH
+## model name and execution
+export model="mymod.py"
+srun -n ${SLURM_NTASKS} python3 $model
