803e2a0605a5078a9911f7642e7a05f6d795f2a5
Julian Giordani <house.of.jules@gmail.com>
2024-12-05 18:28:41 +1100

    Big update of Dockersfiles
    - More automation of versioning in `docker-build.sh`
    - Moving to Ubuntu 24.04 as base OS
    - Update to petsc 3.22.2
    - using `podman` instead of `docker`

--- docs/development/docker/docker-builder.sh
+++ docs/development/docker/docker-builder.sh
@@ -1,35 +1,55 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -e
-
 
 # Example Usage:
 #   - Run from underworld2 repository head
 #   - mpi and lavavu dockers are automatically generated via github actions
 #   - petsc and underworld2 must be created by runn the following script.
 
+UBUNTU_VERSION=24.04
+PYTHON_VERSION=3.12
+OMPI_VERSION=4.1.4
+PETSC_VERSION=3.22.2
 
 ARCH=$(uname -m)
 echo "Will build docker image locally for architecture type: $ARCH"
 echo "************************************************************\n"
 
 # Get the ubuntu image
-docker pull ubuntu:22.04
+podman pull ubuntu:$UBUNTU_VERSION
 
 ## The mpi and lavavu images should be automatically made via github actions
-docker build . -f ./docs/development/docker/mpi/Dockerfile.openmpi -t underworldcode/openmpi:4.1.4-$ARCH
-docker build . -f ./docs/development/docker/lavavu/Dockerfile -t underworldcode/lavavu:$ARCH
+podman build . \
+  -f ./docs/development/docker/mpi/Dockerfile.openmpi \
+  --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
+  --build-arg OMPI_VERSION=$OMPI_VERSION \
+  -t underworldcode/openmpi:$OMPI_VERSION-$ARCH
 
-docker build . \
+podman build \
+  -f ./docs/development/docker/lavavu/Dockerfile \
+  --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
+  --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+  -t underworldcode/lavavu:$ARCH  \
+  .
+
+podman build \
   -f ./docs/development/docker/petsc/Dockerfile \
   --build-arg MPI_IMAGE="underworldcode/openmpi:4.1.4-$ARCH" \
-  -t underworldcode/petsc:3.19.4-$ARCH
+  --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
+  --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+  --build-arg PETSC_VERSION=$PETSC_VERSION \
+  -t underworldcode/petsc:$PETSC_VERSION-$ARCH \
+ .
 
-## don't use pull here as we want the petsc image above
-docker build . \
-  --build-arg PETSC_IMAGE="underworldcode/petsc:3.19.4-$ARCH" \
+### don't use pull here as we want the petsc image above
+podman build . \
+  --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
+  --build-arg PYTHON_VERSION=$PYTHON_VERSION \
+  --build-arg PETSC_IMAGE="underworldcode/petsc:$PETSC_VERSION-$ARCH" \
+  --build-arg LAVAVU_IMAGE="underworldcode/lavavu:$ARCH" \
   -f ./docs/development/docker/underworld2/Dockerfile \
-  -t underworldcode/underworld2:2.15.1b-$ARCH
+  -t underworldcode/underworld2:2.16.0b-$ARCH
 
 
 #docker push underworldcode/petsc:3.19.4-$ARCH 
