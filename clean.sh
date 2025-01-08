35b5c949396689eac3f301186f12bd9ce99ac637
Julian Giordani <julian.giordani@sydney.edu.au>
2025-01-09 10:35:56 +1100

    Updates to conda files, ie latest petsc, numpy<2, cython>=3

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -x
+#!/usr/bin/env bash -x
 
 rm -fr build
 find . -name \*.so -exec rm {} +
 find . -name __pycache__ -exec rm -r {} +
-find -name '*.egg-info' -exec rm -r {} +
+find . -name '*.egg-info' -exec rm -r {} +
 rm -rf .pytest_cache
 ## To remove git 
 #git clean -dfX ; git clean -dfx
