df7a7758470bdc08b4a925b605870dd266a00a31
Julian Giordani <julian.giordani@sydney.edu.au>
2025-01-09 10:32:44 +1100

    Update the dockerfiles

--- docs/development/docker/docker-builder.sh
+++ docs/development/docker/docker-builder.sh
@@ -21,36 +21,37 @@ podman pull ubuntu:$UBUNTU_VERSION
 
 ## The mpi and lavavu images should be automatically made via github actions
 podman build . \
+  --rm --squash-all \
   -f ./docs/development/docker/mpi/Dockerfile.openmpi \
   --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
   --build-arg OMPI_VERSION=$OMPI_VERSION \
   -t underworldcode/openmpi:$OMPI_VERSION-$ARCH
 
-podman build \
+podman build . \
+  --rm --squash-all \
   -f ./docs/development/docker/lavavu/Dockerfile \
   --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
   --build-arg PYTHON_VERSION=$PYTHON_VERSION \
-  -t underworldcode/lavavu:$ARCH  \
-  .
+  -t underworldcode/lavavu:$ARCH
 
-podman build \
+podman build . \
+  --rm --squash-all \
   -f ./docs/development/docker/petsc/Dockerfile \
-  --build-arg MPI_IMAGE="underworldcode/openmpi:4.1.4-$ARCH" \
+  --build-arg MPI_IMAGE="underworldcode/openmpi:$OMPI_VERSION-$ARCH" \
   --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
   --build-arg PYTHON_VERSION=$PYTHON_VERSION \
   --build-arg PETSC_VERSION=$PETSC_VERSION \
-  -t underworldcode/petsc:$PETSC_VERSION-$ARCH \
- .
+  -t underworldcode/petsc:$PETSC_VERSION-$ARCH
 
 ### don't use pull here as we want the petsc image above
 podman build . \
+  --rm --squash-all \
   --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
   --build-arg PYTHON_VERSION=$PYTHON_VERSION \
   --build-arg PETSC_IMAGE="underworldcode/petsc:$PETSC_VERSION-$ARCH" \
   --build-arg LAVAVU_IMAGE="underworldcode/lavavu:$ARCH" \
   -f ./docs/development/docker/underworld2/Dockerfile \
   -t underworldcode/underworld2:2.16.0b-$ARCH
-
 
 #docker push underworldcode/petsc:3.19.4-$ARCH 
 #docker push underworldcode/underworld2:2.15.0b-$ARCH
