dd82dbe33fc1fecbbe8ddd93467e9210797ee862
Julian Giordani <julian.giordani@sydney.edu.au>
2025-01-09 10:43:11 +1100

    Fix typo when calling bash
    use 'set -x' instead

--- clean.sh
+++ clean.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
+
+set -x  # show commands
 
 rm -fr build
 find . -name \*.so -exec rm {} +
 find . -name __pycache__ -exec rm -r {} +
 find . -name '*.egg-info' -exec rm -r {} +
 rm -rf .pytest_cache
-## To remove git 
+## To remove untracked git files
 #git clean -dfX ; git clean -dfx
