984467483c5ff1182c38e9f3322b1fb67aed627a
Julian Giordani <julian.giordani@sydney.edu.au>
2025-10-30 10:31:32 +1100

    More precise file removal in clean.sh

--- clean.sh
+++ clean.sh
@@ -3,9 +3,9 @@
 set -x  # show commands
 
 rm -fr build
-find . -name \*.so -exec rm {} +
-find . -name __pycache__ -exec rm -r {} +
-find . -name '*.egg-info' -exec rm -r {} +
+find src -name '*.so' -exec rm {} +
+find src -name '*.__pycache__' -exec rm -r {} +
+find src -name '*.egg-info' -exec rm -r {} +
 rm -rf .pytest_cache
 ## To remove untracked git files
 #git clean -dfX ; git clean -dfx
