66b65b5c7d80ecd96bb5efb82dd7679c93d227a0
julian <house.of.jules@gmail.com>
2026-05-05 15:33:49 +1000

    fix regex in clean.sh

--- clean.sh
+++ clean.sh
@@ -5,7 +5,7 @@ set -x  # show commands
 rm -fr build
 
 find src -name '*.so' -exec rm {} +
-find src -name '*.__pycache__' -exec rm -r {} +
+find src -name '__pycache__' -exec rm -r {} +
 find src -name '*.egg-info' -exec rm -r {} +
 rm -rf .pytest_cache
 ## To remove untracked git files
